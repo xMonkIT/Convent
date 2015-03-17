f6a87b8ade632090ae29fb9f4ab796da226ef2bb
Владимир <omelin.vladimir@gmail.com>
2015-03-18 02:16:44 +0300

    объединено добавление слайдов и названий презентации

--- ppt_viewer/admin.py
+++ ppt_viewer/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from ppt_viewer.models import Section, Slide, Thesis
 
 # Register your models here.
-admin.site.register(Section)
-admin.site.register(Thesis)
-admin.site.register(Slide)
+
+class SlideInline(admin.TabularInline):
+	model = Slide
+	fields = ['num', 'image']
+	ordering = ['num']
+	extra = 1
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+	search_fields = ['title']
+
+
+@admin.register(Thesis)
+class ThesisAdmin(admin.ModelAdmin):
+	fields = ['section', 'title']
+	search_fields = ['title']
+	inlines = [SlideInline]
+	list_display = ('title', 'section')
